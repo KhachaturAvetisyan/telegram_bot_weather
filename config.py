291706c4e8c4f066c8e2a445dadd6114fd939149
xachoanul44 <72801992+Xacho2002@users.noreply.github.com>
2021-06-03 21:37:27 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "1885745727:AAH76v9XHdFoVAv9-ZBpd_UkttdcQgzdPoA"
+TOKEN = "1885745727:AAGt6WPjk3NsDVOfiQTEmqFvpLEaI-3B5yU"
 TOKEN_OWM = "9b6cbef84565796f8f8e2c2d0f05f5e2"
 
 # https://surik00.gitbooks.io/aiogram-lessons/content/chapter2.html
