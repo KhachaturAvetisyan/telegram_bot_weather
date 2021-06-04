1857c5bbea92a9d31b320d803abc0c29536f4d88
xachoanul44 <72801992+Xacho2002@users.noreply.github.com>
2021-06-04 17:48:20 +0400

    /weather command new func

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pyowm
+import datetime
+
+from pyowm import OWM, commons
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -26,20 +28,63 @@ async def process_help_command(message: types.Message):
 
 @dp.message_handler(commands=['weather'])
 async def process_start_command(msg: types.Message):
-    print(msg.get_args())
+    # print(msg.get_args())
     try:
-        owm = pyowm.OWM(TOKEN_OWM)
+        city = msg.get_args()
+        owm = OWM(TOKEN_OWM)
         mgr = owm.weather_manager()
-
-        observation = mgr.weather_at_place(msg.get_args())
+        observation = mgr.weather_at_place(city)
         w = observation.weather
 
-        temp_cels = w.temperature('celsius')['temp']
-        temp_far = w.temperature('fahrenheit')['temp']
+        # Температура
+        temp = w.temperature('celsius')
+        temp_cels = temp['temp']
+        temp_feels = temp['feels_like']
+        temp_max = temp['temp_max']
+        temp_min = temp['temp_min']
+        # Скорость ветра
+        wind = w.wind()['speed']
+        # Влажность
+        humi = w.humidity
+        # Статус
+        status = w.status
+        # Время
+        time = w.reference_time('iso')
+        # Давление
+        pressure = w.pressure['press']
+        # Видимость
+        vis_dis = w.visibility_distance
+        # Продолжительность дня
+        sunrise_time = datetime.datetime.fromtimestamp(w.sunrise_time())
+        sunset_time = datetime.datetime.fromtimestamp(w.sunset_time())
+        length_of_the_day = sunset_time - sunrise_time
 
-        await msg.reply(f"tempriture in celsius = {temp_cels}°C\ntempriture in farengate = {temp_far}°F", reply=False)
-    except pyowm.commons.exceptions.APIRequestError:
+
+        code_to_smile = {
+            "Clear": "Ясно \U00002600",
+            "Clouds": "Облачно \U00002601",
+            "Rain": "Дождь \U00002614",
+            "Drizzle": "Дождь \U00002614",
+            "Thunderstorm": "Гроза \U000026A1",
+            "Snow": "Снег \U0001F328",
+            "Mist": "Туман \U0001F32B"
+        }
+
+        await msg.reply(
+            f"Время: {time}\n"
+            f"В городе {city.capitalize()} температура: {temp_cels}C°, ощущается как: {temp_feels}C°\n"
+            f"Максимальная температура: {temp_max}C°, минимальная температура: {temp_min}C°.\n"
+            f"Скорость ветера: {wind}м/с\n"
+            f"Влажность: {humi}%\n"
+            f"Давление: {pressure} мм.рт.ст\n"
+            f"Восход солнца: {sunrise_time}\nЗакат солнца: {sunset_time}\nПродолжительность дня: {length_of_the_day}\n"
+            f"Видимость: {vis_dis} м\n"
+            f"Статус: {code_to_smile[status]}", 
+            reply=False)
+
+    except commons.exceptions.APIRequestError:
         await msg.reply("Вы не написали город пожалуйста напишхите рядом с комондой /weather название города. ))", reply=False)
+
     except:
         await msg.reply("Я не знаю что это за город !!))", reply=False)
 
--- weather_consol_app.py
+++ weather_consol_app.py
@@ -1,17 +1,72 @@
-import pyowm
+from pyowm import OWM
+import datetime
 
-city = input("input city - ")
+city = "Erevan"
 
-owm = pyowm.OWM("9b6cbef84565796f8f8e2c2d0f05f5e2")
+owm = OWM("9b6cbef84565796f8f8e2c2d0f05f5e2")
 mgr = owm.weather_manager()
-
 observation = mgr.weather_at_place(city)
 w = observation.weather
 
-temp_cels = w.temperature('celsius')['temp']
-temp_far = w.temperature('fahrenheit')['temp']
+# temp_cels = w.temperature('celsius')['temp']
+# temp_far = w.temperature('fahrenheit')['temp']
 
-# temp_cels = owm.weather_manager().weather_at_place(city).weather.temperature('celsius')['temp']
-# temp_far = owm.weather_manager().weather_at_place(city).weather.temperature('farengate')['temp']
+# Температура
+temp = w.temperature('celsius')
+temp_cels = temp['temp']
+temp_feels = temp['feels_like']
+temp_max = temp['temp_max']
+temp_min = temp['temp_min']
 
-print("tempriture in celsius = " + str(temp_cels) + "\ntempriture in farengate = " + str(temp_far))
+# Скорость ветра
+wind = w.wind()['speed']
+
+# Влажность
+humi = w.humidity
+
+# Облачность
+# clouds = w.clouds
+
+# Статус
+status = w.status
+
+# Детали
+# det = w.detailed_status
+
+# Время
+time = w.reference_time('iso')
+
+# Давление
+pressure = w.pressure['press']
+
+# Видимость
+vis_dis = w.visibility_distance
+
+# Продолжительность дня
+sunrise_time = datetime.datetime.fromtimestamp(w.sunrise_time())
+sunset_time = datetime.datetime.fromtimestamp(w.sunset_time())
+length_of_the_day = sunset_time - sunrise_time
+
+
+code_to_smile = {
+    "Clear": "Ясно \U00002600",
+    "Clouds": "Облачно \U00002601",
+    "Rain": "Дождь \U00002614",
+    "Drizzle": "Дождь \U00002614",
+    "Thunderstorm": "Гроза \U000026A1",
+    "Snow": "Снег \U0001F328",
+    "Mist": "Туман \U0001F32B"
+}
+
+# print("tempriture in celsius = " + str(temp_cels) + "\ntempriture in farengate = " + str(temp_far))
+print(
+	f"Время: {time}\n"
+    f"В городе {city} температура: {temp_cels}C°, ощущается как: {temp_feels}C°\n"
+	f"Максимальная температура: {temp_max}C°, минимальная температура: {temp_min}C°.\n"
+    f"Скорость ветера: {wind}м/с\n"
+    f"Влажность: {humi}%\n"
+	f"Давление: {pressure} мм.рт.ст\n"
+    f"Восход солнца: {sunrise_time}\nЗакат солнца: {sunset_time}\nПродолжительность дня: {length_of_the_day}\n"
+    f"Видимость: {vis_dis} м\n"
+	f"Статус: {code_to_smile[status]}"
+)
